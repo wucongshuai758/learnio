1d52220b72cdcf72af6cbb30e6b6dc7ca2051dc2
congshuai.wcs <congshuai.wcs@alibaba-inc.com>
2019-03-01 17:40:08 +0800

    添加代码

---
+++ src/main/java/com/wcs/learn/Application.java
@@ -0,0 +1,12 @@
+package com.wcs.learn;
+
+/**
+ * @author 吴聪帅
+ * @Description
+ * @Date : 下午5:25 2019/3/1 Modifyby:
+ **/
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("111");
+    }
+}
